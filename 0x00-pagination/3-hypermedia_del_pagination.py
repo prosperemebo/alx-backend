568776a3e9b9e9c757e439ad21ea9eff3f5ed796
prosperemebo <emeboprosper@gmail.com>
2024-05-06 15:15:44 +0100

    New Push

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -36,7 +36,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ Get page at index with information """
+        """Get page at index with information"""
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
